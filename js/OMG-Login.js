ed831d2a047a3e6509a19f806366792f80de47ed
ItsDarkflarx <ItsDarkflarx@github.com>
2020-02-01 15:52:38 +0800

    updated

--- js/OMG-Login.js
+++ js/OMG-Login.js
@@ -31,3 +31,13 @@ var loginDetails = function() {
 	// 	alert("Login Successful!");
 	// }
 }
+
+function showPopup() {
+	var popup = document.getElementById('popup');
+	popup.classList.add('show')
+}
+
+function hidePopup() {
+	var popup = document.getElementById('popup');
+	popup.classList.remove('show')
+}
