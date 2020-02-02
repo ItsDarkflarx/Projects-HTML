ddc67b6628b1aa4cce785b42cca6ed5e3c5b8aed
ItsDarkflarx <ItsDarkflarx@github.com>
2020-02-02 12:22:50 +0800

    updated

--- js/OMG-Login.js
+++ js/OMG-Login.js
@@ -1,22 +1,27 @@
+var regEmail, regPassword, regPassword2;
+var logEmail, logPassword;
+
 var registerDetails = function() {
-	var regEmail, regPassword, regPassword2;
 	regEmail = document.getElementById('regEm').value;
 	regPassword = document.getElementById('regPw').value;
 	regPassword2 = document.getElementById('regPw2').value;
 	// document.getElementById('loginTest').innerHTML=(regEmail);
 	// document.getElementById('pwTest').innerHTML=(regPassword);
 	// document.getElementById('pw2Test').innerHTML=(regPassword2);
+
 	if (regEmail == '' || regPassword == '' || regPassword2 == '') {
 		alert("No Input!");
-	} else if (regPassword != regPassword2 || regPassword2 != regPassword) {
+	} 
+	else if (regPassword != regPassword2 || regPassword2 != regPassword) {
 		alert("Wrong Password!");
-	} else if (regPassword == regPassword2) {
+	} 
+	else if (regPassword == regPassword2) {
 		alert("Register Successful!");
 	} 
+
 }
 
 var loginDetails = function() {
-	var logEmail, logPassword;
 	logEmail = document.getElementById('logEm').value;
 	logPassword = document.getElementById('logPw').value;
 	// document.getElementById('loginTest').innerHTML=(logEmail);
@@ -25,19 +30,20 @@ var loginDetails = function() {
 	if (logEmail == '' || logPassword == '') {
 		alert("No Input!");
 	} 
-	// else if (logEmail != regEmail && logPassword != regPassword) {
-	// 	alert("Wrong Details!");
-	// } else if (logEmail == regEmail && logPassword == regPassword) {} {
-	// 	alert("Login Successful!");
-	// }
+	else if (logEmail != regEmail || logPassword != regPassword) {
+		alert("Wrong Details!");
+	} 
+	else if (logEmail == regEmail && logPassword == regPassword) {
+		alert("Login Successful!");
+	}
 }
 
 function showPopup() {
 	var popup = document.getElementById('popup');
-	popup.classList.add('show')
+	popup.classList.add('show');
 }
 
 function hidePopup() {
 	var popup = document.getElementById('popup');
-	popup.classList.remove('show')
+	popup.classList.remove('show');
 }
